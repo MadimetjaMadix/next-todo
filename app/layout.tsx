65785e2e21c1e592fbcfb3f16b8794a45990c48e
Madimetja.Sethosa <madimetja.sethosa@payat.co.za>
2024-02-17 13:38:03 +0200

    Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
+        <SpeedInsights/>
         <ConvexClientProvider>{children}</ConvexClientProvider>
         </body>
     </html>
